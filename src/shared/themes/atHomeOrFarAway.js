0fed1a6539db3c3188990cef06c7b53d66561688
Simon Eddy <simoneddy@Simons-MacBook-Pro.local>
2019-07-24 20:37:01 +1000

    inverted athome theme

--- src/shared/themes/atHomeOrFarAway.js
+++ src/shared/themes/atHomeOrFarAway.js
@@ -4,9 +4,9 @@ const atHomeOrFarAway = {
   mode: 'atHomeOrFarAway',
   name: 'At Home or Far Away',
   map: {
-    primary: CATS_BLUE,
-    secondary: 'white',
-    primaryTitle: CATS_BLUE
+    primary: 'white',
+    secondary: CATS_BLUE,
+    primaryTitle: 'white'
   }
 };
 
